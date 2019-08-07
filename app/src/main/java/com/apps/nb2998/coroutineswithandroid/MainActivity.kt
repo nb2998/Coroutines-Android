aedc80c32112f0ed974945ffc4c8e701e316fe2f
Nishtha Bhatia <nbhatia329@gmail.com>
2019-08-08 00:03:39 +0530

    Setting dependencies and basic layout

--- app/src/main/java/com/apps/nb2998/coroutineswithandroid/MainActivity.kt
+++ app/src/main/java/com/apps/nb2998/coroutineswithandroid/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.apps.nb2998.coroutineswithandroid
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
