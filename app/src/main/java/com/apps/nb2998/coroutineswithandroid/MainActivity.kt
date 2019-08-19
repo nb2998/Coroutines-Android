89744d90ae1473e5334d84b3030743a38abce7b8
Nishtha Bhatia <nbhatia329@gmail.com>
2019-08-19 16:30:10 +0530

    Comments Explanantion

--- app/src/main/java/com/apps/nb2998/coroutineswithandroid/MainActivity.kt
+++ app/src/main/java/com/apps/nb2998/coroutineswithandroid/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             CoroutineScope(IO).launch {
+                // scope is like grouping a bunch of jobs together (Job 1-5, Job 3 fails- cancel them all)
                 fakeApiCall()
             }
         }
@@ -26,16 +29,29 @@ class MainActivity : AppCompatActivity() {
     private suspend fun fakeApiCall() {
         val result1 = getResult1FromApi()
         logThread("Log: $result1")
+        setTextOnTextView(result1)
     }
 
     //    can be called from within a coroutine
     private suspend fun getResult1FromApi(): String {
         logThread("getResult1FromApi")
-        delay(1000)
+        delay(1000) // diff from Thread.sleep (Only delay this single coroutine and not the entire thread)
         return RESULT1
     }
 
+    private suspend fun setTextOnTextView(text: String) {
+        // possible contexts: IO, Main, Default
+        // IO- N/w requests, DB ; Main- Main thread (interacting with UI) ; Default- For heavy tasks (filter a large list)
+        withContext(Main) { // switch context of coroutine
+            //   are thread independent - can switch threads anytime
+            // launch creates a new coroutine while with context just switches the context
+            tvResult.text = tvResult.text.toString() + "\n$text"
+        }
+    }
+
     private fun logThread(methodName: String) {
+        // Coroutine ≠ Thread (Coroutine is a job to be done and Thread is a place where job will be executed)
+        // hence, multiple coroutines can run on the same thread
         println("Log: $methodName : ${Thread.currentThread().name}")
     }
 }
